0d5fcae1de929c89914254c5f661bd0ec5a4f46d
Karen Pion <karenlpion@gmail.com>
2021-07-21 16:20:58 -0400

    started live server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path')
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
+  );
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    term: 'api',
+    description:
+      'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
+  });
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
